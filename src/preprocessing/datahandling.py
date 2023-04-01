29bffc7d35e13aeaec92454333f5bd19a5f7f7eb
sebastianwidmann <sebastian.widmann@tum.de>
2023-04-01 15:47:29 +0200

    datahandling.py: Modified execution for updated database structure.

--- src/preprocessing/datahandling.py
+++ src/preprocessing/datahandling.py
@@ -27,7 +27,9 @@ def generate_tfrecords(data_dir: str, save_dir: str, stl_format: str,
     Parameters
     ----------
     data_dir : str
-               input directory of vtk files
+               input directory of vtu and stl files. Expects all vtu and stl
+               files to be separated into two folders named 'vtu' and 'stl'
+               with the respective files in each folder.
     save_dir : str
                output directory of TFRecord files
     stl_format : str
@@ -58,7 +60,10 @@ def generate_tfrecords(data_dir: str, save_dir: str, stl_format: str,
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
-    vtu_list = [x for x in sorted(os.listdir(data_dir)) if x.endswith('.vtu')]
+    vtu_folder = os.path.join(data_dir, 'vtu')
+    stl_folder = os.path.join(data_dir, 'stl')
+
+    vtu_list = [x for x in sorted(os.listdir(vtu_folder)) if x.endswith('.vtu')]
     stl_list = [("_".join(x.split("_", 2)[:2]) + ".stl") for x in vtu_list]
 
     dataset = list(zip(vtu_list, stl_list))
@@ -75,14 +80,19 @@ def generate_tfrecords(data_dir: str, save_dir: str, stl_format: str,
                        range(nsamples)]
 
         file_dir = os.path.join(save_dir, 'airfoilMNIST_{}.tfrecord'.format(i))
-
+        
         with tf.io.TFRecordWriter(file_dir) as writer:
             for sample in samples:
-                airfoil, angle, mach, _ = sample[0].split('_')
-                angle, mach = float(angle), float(mach)
+                vtu_dir = os.path.join(vtu_folder, sample[0])
+                stl_dir = os.path.join(stl_folder, sample[1])
 
-                vtu_dir = os.path.join(data_dir, sample[0])
-                stl_dir = os.path.join(data_dir, sample[1])
+                # split string to extract information about airfoil, angle of
+                # attack and Mach number to write as feature into tfrecord
+                sim_config = sample[0].rsplit('.', 1)[0]
+                sim_config = sim_config.split('_')
+
+                airfoil, angle, mach = sim_config
+                angle, mach = float(angle), float(mach)
 
                 data = vtk_to_tfTensor(vtu_dir, stl_dir, stl_format, xmin,
                                        xmax, ymin, ymax, nx, ny, k, p, gpu_id)
