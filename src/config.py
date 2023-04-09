016fa6773ad355ff6a58e04139d049fa990813b8
sebastianwidmann <sebastian.widmann@tum.de>
2023-04-09 02:48:21 +0200

    config.py: Basic layout extended. Incorrect values at the moment!

--- src/config.py
+++ src/config.py
@@ -3,10 +3,13 @@ import ml_collections
 
 def get_config():
     config = ml_collections.ConfigDict()
+    config.batch = 2
+    config.epoch = 1
+    config.patches = (16, 16)
 
     config.preprocess = ml_collections.ConfigDict()
-    config.preprocess.readdir = '../../airfoilMNIST'
-    config.preprocess.writedir = '../../airfoilMNIST/tfrecords'
+    config.preprocess.readdir = '../airfoilMNIST'
+    config.preprocess.writedir = '../airfoilMNIST/tfrecords'
     config.preprocess.nsamples = 10
     config.preprocess.stlformat = 'nacaFOAM'
     config.preprocess.resize = (
@@ -14,5 +17,13 @@ def get_config():
     config.preprocess.resolution = (250, 250)  # new field resolution (nx, ny)
     config.preprocess.num_neighbors = 5
     config.preprocess.gpu_id = 0
+
+    config.transformer = ml_collections.ConfigDict()
+    config.transformer.readdir = config.preprocess.writedir
+    config.transformer.num_layers = 6
+    config.transformer.num_heads = 12
+    config.transformer.dim_mlp = 3072
+    config.transformer.dropout_rate = 0.1
+    config.transformer.att_dropout_rate = 0.1
 
     return config
