6bf8be6fb63830c7ba80676d3bb54bdc1e16bfb9
sebastianwidmann <sebastian.widmann@tum.de>
2023-05-03 13:30:19 +0200

    preprocess.py: TFDS implementation added and algorithm to randomly split dataset into train and test split.

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -1,18 +1,30 @@
+# ----------------------------------------------------------------------------
+# Created By  : Sebastian Widmann
+# Institution : TU Munich, Department of Aerospace and Geodesy
+# Created Date: April 9, 2023
+# version ='1.0'
+# ---------------------------------------------------------------------------
 from absl import app
 from absl import flags
 from ml_collections import config_flags
 import os
 import random
 import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
 from tqdm import tqdm
-
 from conversion import vtk_to_tfTensor, create_tfExample
 
-# from conversion import generate_tfrecords
+from time import sleep
 
 FLAGS = flags.FLAGS
-
-config_flags.DEFINE_config_file('config')
+config_flags.DEFINE_config_file(
+    'config',
+    'config.py',
+    'File path to hyperparameter configuration',
+    lock_config=True,
+)
+flags.mark_flag_as_required('config')
 
 
 def main(argv):
@@ -22,40 +34,50 @@ def main(argv):
     information about TFRecords can be found on
     https://www.tensorflow.org/tutorials/load_data/tfrecord.
     """
-    # parse nested config for preprocessing variables
-    config = FLAGS.config.preprocess
+
+    config = FLAGS.config
 
     # check if output directory exists and create dir if necessary
-    if not os.path.exists(config.writedir):
-        os.makedirs(config.writedir)
+    if not os.path.exists(config.preprocess.writedir):
+        os.makedirs(config.preprocess.writedir)
 
-    vtu_folder = os.path.join(config.readdir, 'vtu')
-    stl_folder = os.path.join(config.readdir, 'stl')
+    vtu_folder = os.path.join(config.preprocess.readdir, 'vtu')
+    stl_folder = os.path.join(config.preprocess.readdir, 'stl')
 
     vtu_list = [x for x in sorted(os.listdir(vtu_folder)) if x.endswith('.vtu')]
     stl_list = [("_".join(x.split("_", 2)[:2]) + ".stl") for x in vtu_list]
 
     dataset = list(zip(vtu_list, stl_list))
 
-    quotient, remainder = divmod(len(dataset), config.nsamples)
-    n_tfrecords = quotient + remainder
+    ds_train = [dataset.pop(random.randrange(len(dataset))) for _ in
+                range(int(config.train_size * len(dataset)))]
+    ds_test = dataset
 
-    for i in tqdm(range(n_tfrecords), desc='TFRecords'):
-        if remainder != 0 and i == n_tfrecords - 1:
-            samples = [dataset.pop(random.randrange(len(dataset))) for _ in
-                       range(remainder)]
+    train_quotient, train_remainder = divmod(len(ds_train),
+                                             config.preprocess.nsamples)
+    test_quotient, test_remainder = divmod(len(ds_test),
+                                           config.preprocess.nsamples)
+
+    n_files_train, n_files_test = (train_quotient + 1), \
+        (test_quotient + 1)
+
+    for i in tqdm(range(n_files_train), desc='Train split', position=0):
+        if train_remainder != 0 and i == n_files_train - 1:
+            batch = [ds_train.pop(random.randrange(len(ds_train))) for _ in
+                     range(train_remainder)]
         else:
-            samples = [dataset.pop(random.randrange(len(dataset))) for _ in
-                       range(config.nsamples)]
+            batch = [ds_train.pop(random.randrange(len(ds_train))) for _ in
+                     range(config.preprocess.nsamples)]
 
-        file_dir = os.path.join(config.writedir,
-                                'airfoilMNIST_{}.tfrecord'.format(i))
+        file_dir = os.path.join(config.preprocess.writedir,
+                                'airfoilMNIST-train.tfrecord-{}-of-{}'.format(
+                                    str(i).zfill(5),
+                                    str(n_files_train).zfill(
+                                        5)))
 
         with tf.io.TFRecordWriter(file_dir) as writer:
-            for sample in samples:
-                vtu_dir = os.path.join(vtu_folder, sample[0])
-                stl_dir = os.path.join(stl_folder, sample[1])
-
+            for sample in tqdm(batch, desc='Shards', position=1,
+                               leave=False):
                 # split string to extract information about airfoil, angle of
                 # attack and Mach number to write as feature into tfrecord
                 sim_config = sample[0].rsplit('.', 1)[0]
@@ -64,11 +86,85 @@ def main(argv):
                 airfoil, angle, mach = sim_config
                 angle, mach = float(angle), float(mach)
 
-                data = vtk_to_tfTensor(vtu_dir, stl_dir, config)
+                vtu_dir = os.path.join(vtu_folder, sample[0])
+                stl_dir = os.path.join(stl_folder, sample[1])
+                data = vtk_to_tfTensor(vtu_dir, stl_dir, config.preprocess)
 
                 example = create_tfExample(airfoil, angle, mach, data)
-
                 writer.write(example.SerializeToString())
+
+    for i in tqdm(range(n_files_test), desc='Test split', position=0):
+        if train_remainder != 0 and i == n_files_test - 1:
+            batch = [ds_test.pop(random.randrange(len(ds_test))) for _ in
+                     range(test_remainder)]
+        else:
+            batch = [ds_test.pop(random.randrange(len(ds_test))) for _ in
+                     range(config.preprocess.nsamples)]
+
+        file_dir = os.path.join(config.preprocess.writedir,
+                                'airfoilMNIST-test.tfrecord-{}-of-{}'.format(
+                                    str(i).zfill(5),
+                                    str(n_files_test).zfill(
+                                        5)))
+
+        with tf.io.TFRecordWriter(file_dir) as writer:
+            for sample in tqdm(batch, desc='Shards', position=1,
+                               leave=False):
+                # split string to extract information about airfoil, angle of
+                # attack and Mach number to write as feature into tfrecord
+                sim_config = sample[0].rsplit('.', 1)[0]
+                sim_config = sim_config.split('_')
+
+                airfoil, angle, mach = sim_config
+                angle, mach = float(angle), float(mach)
+
+                vtu_dir = os.path.join(vtu_folder, sample[0])
+                stl_dir = os.path.join(stl_folder, sample[1])
+                data = vtk_to_tfTensor(vtu_dir, stl_dir, config.preprocess)
+
+                example = create_tfExample(airfoil, angle, mach, data)
+                writer.write(example.SerializeToString())
+
+    # Create metadata files to read dataset with tfds.load(args)
+    features = tfds.features.FeaturesDict({
+        'naca': tfds.features.Text(
+            encoder=None,
+            encoder_config=None,
+        ),
+        'angle': np.float32,
+        'mach': np.float32,
+        'data': tfds.features.Tensor(
+            shape=(12, *config.preprocess.resolution),
+            dtype=np.float32,
+        ),
+    })
+
+    train_shards = [config.preprocess.nsamples for i in range(n_files_train -
+                                                              1)]
+    train_shards.append(train_remainder)
+
+    test_shards = [config.preprocess.nsamples for i in range(n_files_test - 1)]
+    test_shards.append(test_remainder)
+
+    split_infos = [
+        tfds.core.SplitInfo(
+            name='train',
+            shard_lengths=train_shards,
+            num_bytes=0,
+        ),
+        tfds.core.SplitInfo(
+            name='test',
+            shard_lengths=test_shards,
+            num_bytes=0,
+        ),
+    ]
+
+    tfds.folder_dataset.write_metadata(
+        data_dir=config.preprocess.writedir,
+        features=features,
+        split_infos=split_infos,
+        filename_template='{DATASET}-{SPLIT}.{FILEFORMAT}-{SHARD_X_OF_Y}',
+    )
 
 
 if __name__ == '__main__':
