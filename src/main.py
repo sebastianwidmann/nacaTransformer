228dbb6295902f8f683a0531ca5eddc58f5cbec3
sebastianwidmann <sebastian.widmann@tum.de>
2023-05-15 01:37:57 +0200

    main.py: General structure implemented in accordance with FLAX / JAX documentation.

--- src/main.py
+++ src/main.py
@@ -1,29 +1,48 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
 # Created By  : Sebastian Widmann
 # Institution : TU Munich, Department of Aerospace and Geodesy
 # Created Date: March 21, 2023
 # version ='1.0'
 # ---------------------------------------------------------------------------
-"""
 
-"""
-# ---------------------------------------------------------------------------
-
-from os.path import exists
-from sys import exit
-from configparser import ConfigParser
+from absl import app
+from absl import flags
+from absl import logging
+from ml_collections import config_flags
 import tensorflow as tf
+import jax
 
-# Hide any GPUs from Tensorflow. Otherwise, TF might reserve memory and block
-# it for JAX
-tf.config.experimental.set_visible_devices([], 'GPU')
+from src.train import train_and_evaluate
+
+FLAGS = flags.FLAGS
+config_flags.DEFINE_config_file(
+    'config',
+    None,
+    'File path to hyperparameter configuration',
+    lock_config=True,
+)
+flags.mark_flag_as_required('config')
 
 
-def main():
-    return True
+def main(argv):
+    if len(argv) > 1:
+        raise app.UsageError('Too many command-line arguments.')
+
+    # Hide GPUs from TF. Otherwise, TF might reserve memory and block it for JAX
+    tf.config.experimental.set_visible_devices([], 'GPU')
+
+    logging.info('JAX process: %d / %d', jax.process_index(),
+                 jax.process_count())
+    logging.info('JAX local devices: %r', jax.local_devices())
+
+    if FLAGS.config.trainer == 'train':
+        train_and_evaluate(FLAGS.config)
+    elif FLAGS.config.trainer == 'inference':
+        print('Implement inference')
+    else:
+        raise app.UsageError(f'Unknown trainer: {FLAGS.config.trainer}')
 
 
 if __name__ == '__main__':
-    main()
+    jax.config.config_with_absl()
+    app.run(main)
