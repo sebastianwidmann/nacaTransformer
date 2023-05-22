af32a1126ad275a97996bd457d551ebff06cd064
sebastianwidmann <sebastian.widmann@tum.de>
2023-05-22 02:24:11 +0200

    config.py: Updated structure

--- src/config.py
+++ src/config.py
@@ -8,25 +8,24 @@ def get_config():
     # config.trainer = 'preprocess'
     config.train_size = 0.8
     config.batch_size = 2
-    config.num_epochs = 5
-    config.prefetch = 1
-    config.learning_rate = 0.3
+    config.num_epochs = 50
+    config.learning_rate = 0.03
     config.weight_decay = 0.0
 
     config.vit = ml_collections.ConfigDict()
     config.vit.img_size = (200, 200)
     config.vit.patches = (10, 10)
     config.vit.hidden_size = 300  # num_patches^2 * num_channels
-    config.vit.num_layers = 10
-    config.vit.num_heads = 5
+    config.vit.num_layers = 5
+    config.vit.num_heads = 10
     config.vit.dim_mlp = 4 * config.vit.hidden_size
-    config.vit.dropout_rate = 0.0
+    config.vit.dropout_rate = 0.1
     config.vit.att_dropout_rate = 0.0
 
     config.preprocess = ml_collections.ConfigDict()
     config.preprocess.readdir = '/media/sebastianwidmann/Backup500GB/'
     config.preprocess.writedir = '/media/sebastianwidmann/Backup500GB/airfoilMNIST'
-    config.preprocess.nsamples = 9
+    config.preprocess.nsamples = 128
     config.preprocess.dim = (-0.75, 1.25, -1, 1)
     config.preprocess.num_neighbors = 5
     config.preprocess.gpu_id = 0
