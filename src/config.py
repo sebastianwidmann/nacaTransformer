fbb6622dbcdf302cab71f6087da53690ff853473
sebastianwidmann <sebastian.widmann@tum.de>
2023-06-01 10:56:05 +0200

    config.py: Argument added for dataset filtering.

--- src/config.py
+++ src/config.py
@@ -4,28 +4,31 @@ import ml_collections
 def get_config():
     config = ml_collections.ConfigDict()
     config.dataset = '../nacaTransformer/airfoilMNIST/'
-    config.trainer = 'train'
-    # config.trainer = 'preprocess'
+    # config.trainer = 'train'
+    config.trainer = 'preprocess'
     config.train_size = 0.8
-    config.batch_size = 2
-    config.num_epochs = 50
-    config.learning_rate = 0.03
+    config.shuffle_buffer_size = 100
+    config.batch_size = 1024
+    config.num_epochs = 1
+    config.learning_rate = 0.0003
     config.weight_decay = 0.0
 
     config.vit = ml_collections.ConfigDict()
     config.vit.img_size = (200, 200)
     config.vit.patches = (10, 10)
     config.vit.hidden_size = 300  # num_patches^2 * num_channels
-    config.vit.num_layers = 5
-    config.vit.num_heads = 10
+    config.vit.num_layers = 2
+    config.vit.num_heads = 2
     config.vit.dim_mlp = 4 * config.vit.hidden_size
     config.vit.dropout_rate = 0.1
     config.vit.att_dropout_rate = 0.0
 
     config.preprocess = ml_collections.ConfigDict()
-    config.preprocess.readdir = '/media/sebastianwidmann/Backup500GB/'
-    config.preprocess.writedir = '/media/sebastianwidmann/Backup500GB/airfoilMNIST'
-    config.preprocess.nsamples = 128
+    config.preprocess.readdir = ''
+    config.preprocess.writedir = ''
+    config.preprocess.aoa = (-5, 15)
+    config.preprocess.mach = (0, 0.3)
+    config.preprocess.nsamples = 1024
     config.preprocess.dim = (-0.75, 1.25, -1, 1)
     config.preprocess.num_neighbors = 5
     config.preprocess.gpu_id = 0
