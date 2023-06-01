b0bfc1a851ea352ab99537baa7b3b8542d738d0c
sebastianwidmann <sebastian.widmann@tum.de>
2023-06-01 10:54:29 +0200

    main.py: Added environment variable to avoid OOM error on GPU.

--- src/main.py
+++ src/main.py
@@ -11,12 +11,13 @@ from absl import logging
 from ml_collections import config_flags
 import jax
 import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # disable INFO and WARNING messages
 import tensorflow as tf
 
-# from src.preprocessing.preprocess import generate_tfds_dataset
+from src.preprocessing.preprocess import generate_tfds_dataset
 from src.train import train_and_evaluate
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # disable INFO and WARNING messages
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.85"
 
 FLAGS = flags.FLAGS
 config_flags.DEFINE_config_file(
@@ -40,8 +41,7 @@ def main(argv):
     logging.info('JAX local devices: %r', jax.local_devices())
 
     if FLAGS.config.trainer == 'preprocess':
-        print('Uncomment preprocess')
-        # generate_tfds_dataset(FLAGS.config)
+        generate_tfds_dataset(FLAGS.config)
     elif FLAGS.config.trainer == 'train':
         train_and_evaluate(FLAGS.config)
     elif FLAGS.config.trainer == 'inference':
