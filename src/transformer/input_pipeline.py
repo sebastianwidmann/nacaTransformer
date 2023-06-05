7e75d2deee7dfd00eae72b11740bada1cd0e8da7
sebastianwidmann <sebastian.widmann@tum.de>
2023-06-05 21:16:35 +0200

    input_pipeline.py: Removed redundant implementations.

--- src/transformer/input_pipeline.py
+++ src/transformer/input_pipeline.py
@@ -4,7 +4,6 @@
 # Created Date: April 9, 2023
 # version ='1.0'
 # ---------------------------------------------------------------------------
-
 import tensorflow_datasets as tfds
 import tensorflow as tf
 
@@ -12,21 +11,25 @@ import tensorflow as tf
 def get_data_from_tfds(*, config, mode):
     builder = tfds.builder_from_directory(builder_dir=config.dataset)
 
-    dataset = builder.as_dataset(
+    ds = builder.as_dataset(
         split=tfds.split_for_jax_process(mode),
         shuffle_files=True,
     )
 
     if mode == 'train':
-        dataset = dataset.shuffle(config.shuffle_buffer_size,
-                                  seed=0,
-                                  reshuffle_each_iteration=True)
+        ds = ds.shuffle(config.shuffle_buffer_size,
+                        seed=0,
+                        reshuffle_each_iteration=True)
 
-        dataset = dataset.repeat(config.num_epochs)
+        ds = ds.repeat(config.num_epochs)
 
-    dataset = dataset.batch(batch_size=config.batch_size,
-                            drop_remainder=True,
-                            num_parallel_calls=tf.data.AUTOTUNE).prefetch(
-        tf.data.AUTOTUNE)
+    ds = ds.batch(batch_size=config.batch_size,
+                  drop_remainder=True,
+                  num_parallel_calls=tf.data.AUTOTUNE)
 
-    return dataset
+    # # per_host_batch_size = config.batch_size * jax.local_device_count()
+    # # ds = ds.batch(batch_size=per_host_batch_size,
+    # #               drop_remainder=False,
+    # #               num_parallel_calls=tf.data.AUTOTUNE)
+
+    return ds.prefetch(tf.data.AUTOTUNE)
