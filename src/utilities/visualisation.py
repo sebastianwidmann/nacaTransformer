53163dfa10e4fb93f55e693483a0a89e5e02c436
sebastianwidmann <sebastian.widmann@tum.de>
2023-06-05 21:17:43 +0200

    visualisation.py: Added plot implementation to visualise differences between preds and ground truth.

--- src/utilities/visualisation.py
+++ src/utilities/visualisation.py
@@ -59,6 +59,69 @@ def plot_prediction(config: ConfigDict, predictions, ground_truth, epoch, name):
     plt.close()
 
 
+def plot_delta(config, predictions, ground_truth, epoch, cmap):
+    fig, (ax, cax) = plt.subplots(ncols=3, nrows=2, figsize=(15, 6),
+                                  gridspec_kw={"height_ratios": [1, 0.05]})
+
+    xmin, xmax, ymin, ymax = config.preprocess.dim
+    nx, ny = config.vit.img_size
+    x, y = np.mgrid[xmin:xmax:(nx * 1j), ymin:ymax:(ny * 1j)]
+
+    dp = ground_truth[:, :, 0] - predictions[:, :, 0]
+    dux = ground_truth[:, :, 1] - predictions[:, :, 1]
+    duy = ground_truth[:, :, 2] - predictions[:, :, 2]
+
+    im0 = ax[0].pcolormesh(
+        x, y, dp,
+        cmap=cmap,
+        vmin=np.min(dp),
+        vmax=np.max(dp)
+    )
+
+    im1 = ax[1].pcolormesh(
+        x, y, dux,
+        cmap=cmap,
+        vmin=np.min(dux),
+        vmax=np.max(dux)
+    )
+
+    im2 = ax[2].pcolormesh(
+        x, y, duy,
+        cmap=cmap,
+        vmin=np.min(duy),
+        vmax=np.max(duy)
+    )
+
+    # Remove x and y coordinate ticks and labels
+
+    # plt.setp(ax[0].get_xticklabels(), visible=False)
+    # plt.setp(ax[0].get_yticklabels(), visible=False)
+    # plt.setp(ax[1].get_xticklabels(), visible=False)
+    # plt.setp(ax[1].get_yticklabels(), visible=False)
+    # plt.setp(ax[2].get_xticklabels(), visible=False)
+    # plt.setp(ax[2].get_yticklabels(), visible=False)
+    # ax[0].tick_params(axis='both', which='both', length=0)
+    # ax[1].tick_params(axis='both', which='both', length=0)
+    # ax[2].tick_params(axis='both', which='both', length=0)
+
+    plt.colorbar(im0, cax=cax[0], orientation='horizontal',
+                 label='$\Delta p$ [-]',
+                 ticks=np.linspace(np.min(dp), np.max(dp), 5, endpoint=True))
+
+    plt.colorbar(im1, cax=cax[1], orientation='horizontal',
+                 label='$\Delta u_x$ [-]',
+                 ticks=np.linspace(np.min(dux), np.max(dux), 5, endpoint=True))
+
+    plt.colorbar(im2, cax=cax[2], orientation='horizontal',
+                 label='$\Delta u_y$ [-]',
+                 ticks=np.linspace(np.min(duy), np.max(duy), 5, endpoint=True))
+
+    plt.tight_layout()
+
+    plt.savefig('vit_delta_{}.png'.format(epoch), dpi=300)
+    plt.close()
+
+
 def plot_loss(config: ConfigDict, train_loss, test_loss):
     fig, ax = plt.subplots(1, 1, figsize=(5, 5))
 
@@ -81,7 +144,7 @@ def plot_loss(config: ConfigDict, train_loss, test_loss):
     ax.set_title(
         'Epochs = {}, Batch size = {}, Lr = {}, Weight decay = {}'.format(
             config.num_epochs, config.batch_size,
-            config.learning_rate, config.weight_decay), fontsize=8)
+            config.learning_rate_scheduler, config.weight_decay), fontsize=8)
 
     plt.tight_layout()
 
